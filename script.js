631e744de612cb65a23dd2896222beb731f459c6
raymondfanuel <raymondfanuel1@gmail.com>
2025-10-03 18:34:57 +0300

    javascript encryption

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const navLinks = document.getElementById("nav-links");
 toggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+
+function checkPin() {
+const pin = document.getElementById('pinInput').value;
+const errorMsg = document.getElementById('errorMessage');
+      
+if (pin === '1425') {
+  window.location.href="games.html"
+  errorMsg.style.display = 'none';
+} else {
+  errorMsg.textContent = 'Wrong PIN';
+  errorMsg.style.display = 'block';
+  document.getElementById('pinInput').value = ''; // Clear input
+}
+}
+
+function logout() {
+  document.getElementById('loginForm').style.display = 'block';
+  document.getElementById('pageTwo').style.display = 'none';
+  document.getElementById('pinInput').value = '';
+  document.getElementById('errorMessage').style.display = 'none';
+}
