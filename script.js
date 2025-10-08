ff3ab86a5aedfc5e30f1b4c8ce0caeaa92dd408e
raymondfanuel <raymondfanuel1@gmail.com>
2025-10-09 00:48:25 +0300

    version 2

--- script.js
+++ script.js
@@ -32,3 +32,39 @@ try {
 }
 }
 
+
+// counter service js
+
+const counters = document.querySelectorAll('.counter');
+const speed = 300; // smaller = faster
+
+  const startCounting = () => {
+    counters.forEach(counter => {
+      const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const increment = target / speed;
+
+        if (count < target) {
+  counter.innerText = Math.ceil(count + increment);
+  setTimeout(updateCount, 20);
+} else {
+  counter.innerText = target + "+";
+}
+      };
+
+      updateCount();
+    });
+  };
+
+  // Use Intersection Observer so it starts only when visible
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        startCounting();
+        observer.disconnect(); // run once
+      }
+    });
+  });
+
+  observer.observe(document.querySelector('.stats-section'));
